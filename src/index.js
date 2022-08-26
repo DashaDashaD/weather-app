3e4b853a7e9028f41ddfe57941a3f6b4540c6bee
DashaDashaD <111508812+DashaDashaD@users.noreply.github.com>
2022-08-26 17:49:02 +0300

    setAttribite to "src"and "alt"

--- src/index.js
+++ src/index.js
@@ -37,6 +37,12 @@ function showWeather(response) {
   humidityElement.innerHTML = response.data.main.humidity
   let windElement = document.querySelector('#wind')
   windElement.innerHTML = Math.round(response.data.wind.speed)
+  let iconElement = document.querySelector('#icon')
+  iconElement.setAttribute(
+    'src',
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+  )
+  iconElement.setAttribute('alt', response.data.weather[0].description)
 }
 
 function getWeatherbyCity(city) {
